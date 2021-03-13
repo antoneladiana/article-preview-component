249b0b497a8f314ce7363cf3e2ea507e1c411194
Antonela Diana <antoneladiana92@gmai.com>
2021-03-13 10:19:59 -0300

    Version 1

--- javascript/script.js
+++ javascript/script.js
@@ -4,9 +4,13 @@ function toggleShare() {
     var shareSection = document.getElementById("share");
     var profileSection = document.getElementById("profile");
     var articleContent = document.getElementById("article-content");
+    var shareButton = document.querySelector(".share-button");
 
     if(shareSection.style.display === "none") {
         shareSection.style.display = "flex";
+        shareButton.style.backgroundColor = "var(--secondary-color)";
+        shareButton.querySelector(".share-button__icon").style.fill 
+            = "white";
         if(window.screen.width < 800) {
             profileSection.style.display = "none";
             articleContent.style.paddingBottom = "0";
@@ -15,6 +19,9 @@ function toggleShare() {
         shareSection.style.display = "none";
         profileSection.style.display = "flex";
         articleContent.style.paddingBottom = "30px";
+        shareButton.style.backgroundColor = "var(--details-color)";
+        shareButton.querySelector(".share-button__icon").style.fill 
+            = "var(--secondary-color)";
     }
 }
 
